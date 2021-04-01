b3a2150a2aa4d0b2728f9d89ed592b884210c3e5
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-01 22:24:14 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -76,4 +76,15 @@ plot(p224r63_2011$B2_sre)
 plot(p224r63_2011$B3_sre)
 plot(p224r63_2011$B4_sre)
 
+# a quadrat of bands...:
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
 
