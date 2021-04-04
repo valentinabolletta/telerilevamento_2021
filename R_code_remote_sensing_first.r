dede982704041b224499ab68ecfc3cc15529a6e1
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-04 19:15:12 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -143,7 +143,10 @@ plot(p224r63_1988) #visualizzazione delle singole bande (7)
 # B6: infrarosso termico
 # B7: infrarosso medio
 
-#plot in RGB
+#plot in RGB colori naturali
 plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+
+#plot in RGB infrarosso (NIR)
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
 
 
