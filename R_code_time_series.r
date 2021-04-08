e2fff9ce1e2d259e510b38c0b9de7942ba7eed91
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-08 12:29:05 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -6,3 +6,9 @@
 library (raster)
 
 setwd("C:/lab/greenland/")
+
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
