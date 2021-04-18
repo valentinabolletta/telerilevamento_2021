6c72c5eeb577a060f684bc75e1c6c5dc96d3505e
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-18 22:00:59 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,3 +3,8 @@
 setwd("C:/lab/")
 
 library(knitr)
+
+# starting from the code folder where framed.sty is put!
+
+stitch("~/Downloads/R_code_temp.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
