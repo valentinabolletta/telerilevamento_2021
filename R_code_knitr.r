51466640858bdfcaabf53a9fc18fbce9561a1a04
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-18 22:34:11 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -6,5 +6,5 @@ library(knitr)
 
 # starting from the code folder where framed.sty is put!
 
-stitch("~/Downloads/R_code_temp.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("codice time series greenland", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
