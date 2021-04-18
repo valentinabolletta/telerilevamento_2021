6c8cfd06e2668b25bf4df1ed4011c860e5843bfe
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-18 14:57:57 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -8,3 +8,9 @@ library(ncdf4)
 setwd("C:/lab/")
 
 albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
+albedo
+
+cl<- colorRampPalette(c('blue','green','red','yellow')) (100)#
+plot (albedo,col=cl)
+
+                      
