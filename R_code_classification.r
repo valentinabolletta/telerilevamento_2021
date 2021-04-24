50ff26f8bce6ce77e5ced655f86d1582a879de5c
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-24 10:49:43 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -39,3 +39,6 @@ gc<-brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
