d1d0f1464e75ebb780660962f2e25e1bf9d9a837
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-04-29 12:23:34 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -5,3 +5,14 @@ setwd("c:/lab/")
 
 defor1 <- brick("defor1_.jpg") 
 defor2 <- brick("defor2_.jpg") 
+
+# band1: NIR, defor1_.1
+# band2: red, defor1_.2
+# band3: green
+
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+par(mfrow=c(1,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
