f91b82f0a772baeeb6b1f1e4c39fb55ce0f70309
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-05-01 10:28:34 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -18,4 +18,6 @@ plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 
 defor1 #per vedere quali sono i nomi delle bande da inserire
+dvi1 <- defor1$defor1.1 - defor1$defor1.2 
+
 
