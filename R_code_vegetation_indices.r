21a3b267c90c357765d5a5fc24b6bdec1408ef50
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-05-01 11:04:36 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -20,6 +20,8 @@ plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 defor1 #per vedere quali sono i nomi delle bande da inserire
 
 #difference vegetation index
+
+#time 1
 dvi1 <- defor1$defor1.1 - defor1$defor1.2 
 plot(dvi1)
 
@@ -27,6 +29,14 @@ plot(dvi1)
 plot(dvi1)
 
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
-par(mfrow=c(1,2))
 plot(dvi1, col=cl,main="DVI1 at time 1)
+
+
+#time 2
+
+dvi2 <- defor2$defor2.1 - defor2$defor2.2 
+plot(dvi2, col=cl, main="DVI1 at time 2)
+
+par(mfrow=c(1,2))
+plot(dvi1, col=cl)
 plot(dvi2, col=cl)
