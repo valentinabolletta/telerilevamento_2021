92be7eb698d61f1fd0b8c8573a8a481a80a7e6c5
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-05-07 22:52:15 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -7,3 +7,13 @@ library(ggplot2)
 
 setwd("c:/lab/")
 
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+
+defor1
+# names: defor1_.1, defor1_.2, defor1_.3 
+# defor1_.1 = NIR
+# defor1_.2 = red
+# defor1_.3 = green
+
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
