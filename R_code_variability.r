7bbcaf88435ba9ca8f61079f820d4994c7b6c4d8
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-07-08 17:49:29 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -2,9 +2,9 @@
 
 library(raster)
 library(RStoolbox)
-library(ggplot2)
-library(gridExtra)
-library(viridis)
+library(ggplot2) #for ggplot plotting
+library(gridExtra) #for plotting ggplot together
+library(viridis) #for ggplot couloring
 
 setwd("c:/lab/")
 
