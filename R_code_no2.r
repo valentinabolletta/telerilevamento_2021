21f98a22565356dce729552eae8a1289c373bec2
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-07-16 14:49:32 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -1,1 +1,12 @@
-R_code_no2.r
+#R_code_no2.r
+
+setwd("C:/lab/EN") # Windows
+library(raster)
+
+# 2. Import the first image (single band)
+# we will select band 1, but the raster function enables to select other single-bands
+# [suggerimento studenti]
+# https://www.rdocumentation.org/packages/raster/versions/3.4-10/topics/raster
+
+EN01 <- raster("EN_0001.png")
+
