8b35575fdac363fa79e2f35f4909c9af3481d9d7
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-07-16 09:38:31 +0200

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -235,6 +235,32 @@ plot(melt_amount, col=clb)
 
 levelplot(melt_amount, col.regions=clb)
 
+#..........................................................
+
+#3. R code Copernicus
+
+# R_code_copernicus.r
+# Visualizing Copernicus data
+
+#install.packages("ncdf4")
+library(raster)
+library(ncdf4)
+
+setwd("C:/lab/")
+
+albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
+albedo
+
+cl<- colorRampPalette(c('blue','green','red','yellow')) (100)#
+plot (albedo,col=cl)    #posso fare la stessa operazione con un altro dato scaricato "FAPAR"
+
+#resampling
+albedores<- aggregate(albedo,fact=100)
+plot(albedores,col=cl)
+
+
+                      
+
 
 
 
