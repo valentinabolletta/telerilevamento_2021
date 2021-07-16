8e5fc509e35b3bcc358c363320ac1d8fc34711fa
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-07-16 10:29:15 +0200

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -11,6 +11,7 @@
 #7. R code ggplot2
 #8. R code vegetation indices
 #9. R code land cover
+#10. R code variability
 
 #.........................................
 
@@ -556,6 +557,98 @@ p1<-ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(st
 grid.arrange(p1, p2, nrow = 1) # this needs griExtra       
           
 #..........................................................
+          
+#10. R code variability
+#R_code_variability.r
+
+library(raster)
+library(RStoolbox)
+library(ggplot2) #for ggplot plotting
+library(gridExtra) #for plotting ggplot together
+library(viridis) #for ggplot couloring
+
+setwd("c:/lab/")
+
+sent <- brick("sentinel.png")
+sent
+
+#NIR 1, RED 2, GREEN 3
+#r=1, g=2, b=3
+
+plotRGB(sent) 
+
+plotRGB(sent, stretch="lin")
+plotRGB(sent, r=2, g=1, b=3, stretch="lin")
+
+nir <- sent$sentinel.1
+red <- sent$sentinel.2
+
+ndvi <- (nir-red) / (nir+red)
+plot(ndvi)
+
+cl <- colorRampPalette(c('black','white','red','magenta','green'))(100) # 
+plot(ndvi,col=cl)
+
+ndvisd3 <- focal(ndvi, w=matrix(1/9,nrow=3,ncol=3), fun=sd)
+plot(ndvisd3)
+
+
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #
+plot(ndvisd3, col=clsd)
+ 
+#mean ndvi with focal
+ndvimean3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=mean)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(ndvimean3, col=clsd)
+
+ndvisd5 <- focal(ndvi, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(ndvisd5, col=clsd)
+
+#PCA
+sentpca<- rasterPCA(sent)
+plot(sentpca$map)
+
+summary(sentpca$model)
+#the first PC contains 0.6736804 of the original information
+ 
+
+pc1 <- sentpca$map$PC1
+
+pc1sd5 <- focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(pc1sd5, col=clsd)
+
+# pc1 <- sentpca$map$PC1
+# pc1sd7 <- focal(pc1, w=matrix(1/49, nrow=7, ncol=7), fun=sd)
+# plot(pc1sd7)
+
+# With the source function you can upload code from outside!
+source("source_test_lezione.r")
+source("source_ggplot.r")
+
+# https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+# The package contains eight color scales: “viridis”, the primary choice, and five alternatives with similar properties - “magma”, “plasma”, “inferno”, “civids”, “mako”, and “rocket” -, and a rainbow color map - “turbo”.
+
+p1 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis()  +
+ggtitle("Standard deviation of PC1 by viridis colour scale")
+
+p2 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "magma")  +
+ggtitle("Standard deviation of PC1 by magma colour scale")
+
+p3 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "turbo")  +
+ggtitle("Standard deviation of PC1 by turbo colour scale")
+
+grid.arrange(p1, p2, p3, nrow = 1)
+
+#.....................................
+          
           
           
 
