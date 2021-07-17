1b7cd55939fdac8315011224f5a211df23fca7ff
valentinabolletta <81106687+valentinabolletta@users.noreply.github.com>
2021-07-17 11:07:39 +0200

    Update R_code_spectral_signatures.r

--- R_code_spectral_signatures.r
+++ R_code_spectral_signatures.r
@@ -8,3 +8,9 @@ defor2 <- brick("defor2.jpg")
 
 #defor2.1, defor2.2, defor2.3  
 # NIR, red, green
+
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+#or
+plotRGB(defor2, r=1, g=2, b=3, stretch="hist")
+ 
+
